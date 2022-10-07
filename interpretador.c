fed219f6c5363d38330e6e3e01ff9e55dd787811
Rafael Paladini <56520474+PaMeirelles@users.noreply.github.com>
2022-10-07 12:40:40 +0000

    command line -> struct

--- interpretador.c
+++ interpretador.c
@@ -8,46 +8,49 @@ s_processo * carrega_processo(char * linha){
   char buffer[100];
   int i;
   s_processo * processo = malloc(sizeof(s_processo));
-
+  
   indices[0] = strstr(linha, "exec ");
   indices[1] = strstr(linha,  ", prioridade=");
   indices[2] = strstr(linha,  ", inicio_tempo_execucao=");
   indices[3] = strstr(linha,  ", tempo_total_execucao=");
-
-  for(int i=0; i < 4; i++){
-    if(indices[i] == NULL){
-      return NULL;
+  printf("%p %p %p %p\n", indices[0], indices[1], indices[2], indices[3]);
+  
+  for(int j=0; j < 4; j++){
+    if(!indices[j]){
+      printf("Erro de sintaxe no argumento %d\n", j+1);
+      exit(-j);
     }
   }
+  
   i = indices[1] - indices[0] - 5;
   processo->nome = malloc(i);
   for(int j=0; j < i; j++){
     processo->nome[j] = *(indices[0] + 5 + j);
   }
-
+  
   i = indices[2] - indices[1] - 13;
   for(int j=0; j < i; j++){
     buffer[j] = *(indices[i] + 13 + j);
   }
   processo->prio = atoi(buffer);
-
+  
   i = indices[3] - indices[2] - 24;
   for(int j=0; j < i; j++){
     buffer[j] = *(indices[i] + 24 + j);
   }
   buffer[i] = '\0';
   processo->inicio = atoi(buffer);
-
-  char * p = indices[3];
+  
+  char * p = indices[3]+23;
   int counter = 0;
-  while(p){
+  while(*p){
     buffer[counter] = *p;
     counter++;
     p++;
   }
   buffer[counter] = '\0';
   processo->duracao = atoi(buffer);
-
+  
   return processo;
 }
 
--- main.c
+++ main.c
@@ -3,12 +3,8 @@
 #include "interpretador.h"
 
 int main(void) {
-  s_processo * processo = malloc(sizeof(s_processo));
-
-  processo->nome = "teste";
-  processo->prio = 3;
-  processo->inicio = 15;
-  processo->duracao = 20;
+  char * linha = "exec p1, prioridade=3, inicio_tempo_execucao=10, tempo_total_execucao=8";
+  s_processo * processo = carrega_processo(linha);
 
   printa_processo(processo);
 }
