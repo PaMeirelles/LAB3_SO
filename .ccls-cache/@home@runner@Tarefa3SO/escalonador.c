fbbf6ce3fe5c94e77ca4d5d93db95b33c658a4a8
Rafael Paladini <56520474+PaMeirelles@users.noreply.github.com>
2022-10-08 21:51:46 +0000

    SIGKILL

--- .ccls-cache/@home@runner@Tarefa3SO/escalonador.c
+++ .ccls-cache/@home@runner@Tarefa3SO/escalonador.c
@@ -7,30 +7,31 @@
 #include <signal.h>
 
 void escalona(struct timeval * inicio, struct timeval * ultima_mod, struct timeval * ultimo_inc, s_no_prio * base, s_no_processo ** running){
+  // variaveis
   s_no_prio * p = base;
   struct timeval agora;
   int over = 0;
+  // checa se uma UT se passou
   gettimeofday(&agora, NULL);
-  
-  if((agora.tv_sec - ultimo_inc->tv_sec) < 1){
+  if((agora.tv_sec - ultimo_inc->tv_sec) < 1 && *running != NULL){
     return;
-  }
-  if(*running != NULL){
-    
-  printf("Tempo: %d Rodando: %s\n", agora.tv_sec - inicio->tv_sec, (*running)->processo->nome);
-  }
-
+    }
   gettimeofday(ultimo_inc, NULL);
+
+  // printa report
   if(*running != NULL){
       (*running)->processo->decorrido += 1;
-  }
-
-  if((*running) != NULL &&   (*running)->processo->decorrido >=   (*running)->processo->duracao){
-    remove_processo((*running)->processo->nome, (*running)->processo->prio, base);
+    }
+  // Se um processo rodou tudo que precisava, removemos ele
+  if((*running) != NULL &&   (*running)->processo->decorrido >= (*running)->processo->duracao){
+    char * nome = (*running)->processo->nome;
+    unsigned short prio = (*running)->processo->prio;
+    kill((*running)->processo->id, SIGKILL);
+     remove_processo(nome, prio, base);
     over = 1;
     *running = NULL;
     }
-
+  // Encontramos o nível de prioridade não-vazio mais alto, e encerramos o programa caso não haja
   while(1){
     if(p == NULL){
       printf("Fim do programa\n");
@@ -44,38 +45,45 @@ void escalona(struct timeval * inicio, struct timeval * ultima_mod, struct timev
     }
   }
 
+  // Se o próximo da fila era o próprio processo já rodando, retornamos
+  if(!over && (*running) == p->no->next && (*running) != NULL){
+    gettimeofday(ultima_mod, NULL);
 
+      printf("Tempo: %d Rodando: %s\n", agora.tv_sec - inicio->tv_sec, (*running)->processo->nome);    return;
+    }
+  
+  // Se a maior prioridade está no mesmo nível do anterior e ainda não acabou o time slice, retornamos
     if((*running) != NULL && (agora.tv_sec - ultima_mod->tv_sec) % 3 != 0 && (*running)->processo->prio == p->no->next->processo->prio){
+
+      printf("Tempo: %d Rodando: %s\n", agora.tv_sec - inicio->tv_sec, (*running)->processo->nome);
     return;
   }
 
-
-  if(!over && (*running) == p->no->next && (*running) != NULL){
-    return;
-    }
-
-
+  // Interrompemos o processo que executava
   if((*running) != NULL){
     kill((*running)->processo->id, SIGSTOP);
-    //printf("Interrompe %s\n", (*running)->processo->nome);
    (*running)->processo->state = 3;
+    // Se o processo que rodava se encontra na maior prioridade, damos a vez ao proximo
     if((*running) == p->no){
         p->no = p->no->next;
     }
   }
 
+  // Caso o processo nunca tenha sido executado, o criamos
   if(p->no->processo->state == 1){
-    //printf("Inicia processo %s\n", p->no->processo->nome);
+
     acha_processo(p->no->processo->nome, p->no->processo->prio, base)->processo->id = getpid();
 
     if(fork() != 0){
       execve(p->no->processo->nome, NULL, NULL);
     }
   }
+  // Caso esteja em espera, retomamos
   else if(p->no->processo->state == 3){
     kill(p->no->processo->id, SIGCONT);
-    //printf("Retoma processo %s\n", p->no->processo->nome);
+
   }
+  // Se chegamos aqui, deu merda em algum lugar
   else{
     printf("Deu merda %d\n", p->no->processo->state);
     exit(1);
@@ -84,5 +92,7 @@ void escalona(struct timeval * inicio, struct timeval * ultima_mod, struct timev
 
   p->no->processo->state = 2;
   *running = p->no;
+
+      printf("Tempo: %d Rodando: %s\n", agora.tv_sec - inicio->tv_sec, (*running)->processo->nome);
   return;
 }
